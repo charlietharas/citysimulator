78cc7c58c37e61c703efe1278077cfb4a68edf86
Charlie Tharas <charlie@charliemax.dev>
2023-11-28 12:57:30 -0500

    Jim bugfix

--- Cow.java
+++ Cow.java
@@ -1,4 +1,5 @@
 package sim;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -9,9 +10,9 @@ import java.io.*;
 class Vector2 {
     double x;
     double y;
-    
+
     public String toString() { return "(" + this.x + ", " + this.y + ")"; }
-    
+
     Vector2() {} // NOTE: x and y automatically initialized to zero
     Vector2(double x, double y) {
         this.x = x;
@@ -25,7 +26,7 @@ class Vector2 {
         this.x = s;
         this.y = s;
     }
-    
+
     Vector2 plus(Vector2 other) { return new Vector2(this.x + other.x, this.y + other.y); }
     Vector2 minus(Vector2 other) { return new Vector2(this.x - other.x, this.y - other.y); }
     Vector2 times(double scalar) { return new Vector2(scalar * this.x, scalar * this.y); }
@@ -33,11 +34,11 @@ class Vector2 {
     double squaredLength() { return this.x * this.x + this.y * this.y; }
     double length() { return Math.sqrt(this.squaredLength()); }
     Vector2 directionVector() { return this.dividedBy(this.length()); }
-    
+
     static double distanceBetween(Vector2 a, Vector2 b) { return (b.minus(a)).length(); }
     static Vector2 directionVectorFrom(Vector2 a, Vector2 b) { return (b.minus(a)).directionVector(); }
     static Vector2 lerp(double t, Vector2 a, Vector2 b) { return a.times(1.0 - t).plus(b.times(t)); }
-    
+
     static final Vector2 right = new Vector2( 1.0,  0.0);
     static final Vector2 left  = new Vector2(-1.0,  0.0);
     static final Vector2 up    = new Vector2( 0.0,  1.0);
@@ -48,11 +49,11 @@ class Vector3 {
     double x;
     double y;
     double z;
-    
+
     public String toString() { return "(" + this.x + ", " + this.y + ", " + this.z + ")"; }
-    
+
     Vector3() { }
-    
+
     Vector3(double x, double y, double z) {
         this.x = x;
         this.y = y;
@@ -68,12 +69,14 @@ class Vector3 {
         this.y = s;
         this.z = s;
     }
+    
+    // ADDED BY CHARLIE
     Vector3(String hex) {
     	this.x = (double)Integer.parseInt(hex.substring(0, 2), 16)/255.0;
     	this.y = (double)Integer.parseInt(hex.substring(2, 4), 16)/255.0;
     	this.z = (double)Integer.parseInt(hex.substring(4, 6), 16)/255.0;
     }
-    
+
     Vector3 plus(Vector3 other) { return new Vector3(this.x + other.x, this.y + other.y, this.z + other.z); }
     Vector3 minus(Vector3 other) { return new Vector3(this.x - other.x, this.y - other.y, this.z - other.z); }
     Vector3 times(double scalar) { return new Vector3(scalar * this.x, scalar * this.y, scalar * this.z); }
@@ -81,9 +84,11 @@ class Vector3 {
     double squaredLength() { return this.x * this.x + this.y * this.y + this.z * this.z; }
     double length() { return Math.sqrt(this.squaredLength()); }
     Vector3 directionVector() { return this.dividedBy(this.length()); }
-    
+
+    static double distanceBetween(Vector3 a, Vector3 b) { return (b.minus(a)).length(); }
+    static Vector3 directionVectorFrom(Vector3 a, Vector3 b) { return (b.minus(a)).directionVector(); }
     static Vector3 lerp(double t, Vector3 a, Vector3 b) { return a.times(1.0 - t).plus(b.times(t)); }
-    
+
     static final Vector3 white     = new Vector3(1.0 , 1.0 , 1.0 );
     static final Vector3 lightGray = new Vector3(0.75, 0.75, 0.75);
     static final Vector3 gray      = new Vector3(0.5 , 0.5 , 0.5 );
@@ -99,11 +104,68 @@ class Vector3 {
     static Vector3 rainbowSwirl(double time) {
         return new Vector3(_rainbowSwirlHelper(time, 0.0), _rainbowSwirlHelper(time, 0.33), _rainbowSwirlHelper(time, -0.33));
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    
+
     static double _rainbowSwirlHelper(double time, double offset) {
         return 0.5 + 0.5 * Math.cos(6.28 * (offset - time));
+    }
+}
+
+class ExampleApp extends App {
+    Vector2 chaserPosition;
+    double time;
+
+    void setup() {
+        chaserPosition = new Vector2();
+        time = 0.0;
+    }
+
+    void loop() {
+        if (!keyToggled('P')) { time += 0.0167; }
+        if (mouseHeld) {
+            chaserPosition = chaserPosition.plus(Vector2.directionVectorFrom(chaserPosition, mousePosition));
+        }
+        drawLine(chaserPosition, mousePosition, Vector3.white);
+        drawCircle(chaserPosition, 2.0, Vector3.rainbowSwirl(time));
+        drawCenterRectangle(mousePosition, new Vector2(4.0), Vector3.cyan);
+    }
+
+    static void demo() {
+        App app = new ExampleApp();
+        app.setWindowBackgroundColor(Vector3.black);
+        app.setWindowSizeInWorldUnits(64.0, 64.0);
+        app.setWindowCenterInWorldUnits(0.0, 0.0);
+        app.setWindowHeightInPixels(512);
+        app.setWindowTopLeftCornerInPixels(64, 64);
+        app.run();
+    }
+
+    public static void main(String[] arguments) {
+        ExampleApp.demo();
+    }
+}
+
+class CoordinatesApp extends App {
+    void setup() {
+    }
+
+    void loop() {
+        drawLine(new Vector2(0.0), new Vector2(9.0), Vector3.white);
+    }
+
+    static void demo() {
+        App app = new CoordinatesApp();
+        app.setWindowBackgroundColor(Vector3.gray);
+        app.setWindowSizeInWorldUnits(9.0, 9.0);
+        app.setWindowCenterInWorldUnits(4.5, 4.5);
+        app.setWindowHeightInPixels(512);
+        app.setWindowTopLeftCornerInPixels(64, 64);
+        app.run();
+    }
+
+    public static void main(String[] arguments) {
+        CoordinatesApp.demo();
     }
 }
 
@@ -118,7 +180,7 @@ class App extends JPanel {
         setWindowHeightInPixels(_windowHeightInPixels);
         setWindowTopLeftCornerInPixels(_windowTopLeftCornerXInPixels, _windowTopLeftCornerYInPixels);
         _jFrame.setVisible(true);
-        
+
         while (!(hotkeysEnabled && keyPressed('q'))) {
             this.repaint();
             try { Thread.sleep(1000 / 60); } catch (Exception e) { }
@@ -126,7 +188,7 @@ class App extends JPanel {
         System.exit(0);
     }
     void reset() { _resetCalled = true; }
-    
+
     // draw
     void drawString(String string, Vector2 _position, Vector3 color, int fontSize, boolean center) {
         // Suppress Mac warnings about missing Times and Lucida.
@@ -152,7 +214,7 @@ class App extends JPanel {
     void drawCircle(Vector2 center, double radius, Vector3 color) { _drawCenterShape(center, new Vector2(2 * radius), color, 1); }
     void drawCenterRectangle(Vector2 center, Vector2 size, Vector3 color) { _drawCenterShape(center, size, color, 0); }
     void drawCornerRectangle(Vector2 _cornerA, Vector2 _cornerB, Vector3 color) { _drawCornerShape(_cornerA, _cornerB, color, 0); }
-    
+
     // input
     Vector2 mousePosition;
     boolean mousePressed = false;
@@ -164,7 +226,7 @@ class App extends JPanel {
     boolean keyToggled(int key) { return _keyToggled.getOrDefault(_keyMakeCaseInvariant(key), false); }
     boolean keyAnyPressed;
     char keyLastPressed;
-    
+
     // window
     void setWindowBackgroundColor(double r, double g, double b) {
         setWindowBackgroundColor(new Vector3(r, g, b));
@@ -176,10 +238,12 @@ class App extends JPanel {
     void setWindowSizeInWorldUnits(double width, double height) {
         _windowWidthInWorldUnits = width;
         _windowHeightInWorldUnits = height;
-        
+
         _windowWidthInPixels = (int) (_windowPixelsPerWorldUnit() * _windowWidthInWorldUnits);
         _windowHeightInPixels = (int) (_windowPixelsPerWorldUnit() * _windowHeightInWorldUnits);
-        _jFrame.setSize(_windowWidthInPixels, _windowHeightInPixels);
+        // _jFrame.setSize(_windowWidthInPixels, _windowHeightInPixels);
+        this.setPreferredSize(new Dimension(_windowWidthInPixels, _windowHeightInPixels));
+        _jFrame.pack();
     }
     void setWindowCenterInWorldUnits(double x, double y) {
         _windowCenterXInWorldUnits = x;
@@ -187,25 +251,27 @@ class App extends JPanel {
     }
     void setWindowHeightInPixels(int height) {
         _windowHeightInPixels = height;
-        
+
         _windowWidthInPixels = (int) (_windowPixelsPerWorldUnit() * _windowWidthInWorldUnits);
-        _jFrame.setSize(_windowWidthInPixels, _windowHeightInPixels);
+        // _jFrame.setSize(_windowWidthInPixels, _windowHeightInPixels);
+        this.setPreferredSize(new Dimension(_windowWidthInPixels, _windowHeightInPixels));
+        _jFrame.pack();
     }
     void setWindowTopLeftCornerInPixels(int x, int y) {
         _windowTopLeftCornerXInPixels = x;
         _windowTopLeftCornerYInPixels = y;
         _jFrame.setLocation(x, y);
     }
-    
+
     boolean hotkeysEnabled = true;
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    
+
     public class NullOutputStream extends OutputStream {
         @Override
         public void write(int b) throws IOException {}
     }
-    
+
     Vector3 _windowBackgroundColor = Vector3.white;
     double _windowWidthInWorldUnits  = 16.0;
     double _windowHeightInWorldUnits = 16.0;
@@ -215,7 +281,7 @@ class App extends JPanel {
     int _windowWidthInPixels;
     int _windowTopLeftCornerXInPixels = 256;
     int _windowTopLeftCornerYInPixels = 64;
-    
+
     double _windowPixelsPerWorldUnit() { return _windowHeightInPixels / _windowHeightInWorldUnits; }
     Vector2 _windowPixelFromWorld(Vector2 sWorld) {
         Vector2 sPixel = new Vector2();
@@ -231,7 +297,7 @@ class App extends JPanel {
         sWorld.y = _windowHeightInWorldUnits - (sPixel.y / scale) + (_windowCenterYInWorldUnits - .5 * _windowHeightInWorldUnits);
         return sWorld;
     }
-    
+
     Color _graphicsColorFromVector3(Vector3 color) {
         return new Color((float) color.x, (float) color.y, (float) color.z);
     }
@@ -245,11 +311,11 @@ class App extends JPanel {
     void _drawCornerShape(Vector2 _cornerA, Vector2 _cornerB, Vector3 color, int shapeType) {
         Vector2 cornerA = _windowPixelFromWorld(_cornerA);
         Vector2 cornerB = _windowPixelFromWorld(_cornerB);
-        
+
         // swap if necessary to make A lower-left and B upper-right
         if (cornerA.x > cornerB.x) { double tmp = cornerA.x; cornerA.x = cornerB.x; cornerB.x = tmp; }
         if (cornerA.y > cornerB.y) { double tmp = cornerA.y; cornerA.y = cornerB.y; cornerB.y = tmp; }
-        
+
         _graphicsSetColor(color);
         int arg0 = (int) (cornerA.x);
         int arg1 = (int) (cornerA.y);
@@ -266,18 +332,18 @@ class App extends JPanel {
         int nPoints = points.length;
         int[] xPoints = new int[nPoints];
         int[] yPoints = new int[nPoints];
-        
+
         for (int i = 0; i < nPoints; ++i) {
             Vector2 tmp = _windowPixelFromWorld(points[i]);
             xPoints[i] = (int) tmp.x;
             yPoints[i] = (int) tmp.y;
         }
-        
+
         _graphicsSetColor(color);
         _graphics.drawPolyline(xPoints, yPoints, nPoints);
     }
-    
-    
+
+
     JFrame _jFrame;
     Hashtable<Integer, Boolean> _keyPressed = new Hashtable<>();
     Hashtable<Integer, Boolean> _keyHeld = new Hashtable<>();
@@ -289,11 +355,11 @@ class App extends JPanel {
         }
         return key;
     }
-    
-    
+
+
     App() {
         super();
-        
+
         {
             this.addMouseListener( new MouseAdapter() {
                 @Override
@@ -301,13 +367,13 @@ class App extends JPanel {
                     mousePressed = true;
                     mouseHeld = true;
                 }
-                
+
                 @Override public void mouseReleased(MouseEvent e) {
-                    mouseHeld = false;
-                    mouseReleased = true;
+                mouseHeld = false;
+                mouseReleased = true;
                 }
             });
-            
+
             KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(event -> {
                 synchronized (App.class) {
                     int key = event.getKeyCode();
@@ -335,29 +401,31 @@ class App extends JPanel {
         }
         {
             _jFrame = new JFrame("CS136");
+            _jFrame.add(this);
             _jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             _jFrame.getContentPane().add(this, BorderLayout.CENTER);
+            // _jFrame.setUndecorated(true);
         }
     }
-    
+
     boolean _resetCalled = false;
     boolean _initialized = false;
     Graphics _graphics;
-    
+
     @Override 
     public void paintComponent(Graphics _graphics) {
         // NOTE: try-catch to actually kill the app on an error
         try {
             super.paintComponent(_graphics);
             this._graphics = _graphics;
-            
+
             // XXX: resizeable window
             // {
             //     Rectangle rectangle = _jFrame.getBounds();
             //     _windowHeightInPixels = rectangle.height;
             //     _windowWidthInPixels = rectangle.width;
             // }
-            
+
             {
                 Point point;
                 {
@@ -366,12 +434,12 @@ class App extends JPanel {
                 }
                 this.mousePosition = _windowWorldFromPixel(new Vector2(point.x, point.y));
             }
-            
+
             if (!_initialized || (hotkeysEnabled && keyPressed('r')) || _resetCalled) {
                 _initialized = true;
                 _resetCalled = false;
                 setup();
-                
+
                 mousePressed = false;
                 mouseHeld = false;
                 mouseReleased = false;
@@ -382,9 +450,9 @@ class App extends JPanel {
                 keyAnyPressed = false;
                 keyLastPressed = 0;
             }
-            
+
             loop();
-            
+
             { // end of _jFrame
                 mousePressed = false;
                 mouseReleased = false;
@@ -397,7 +465,7 @@ class App extends JPanel {
             System.exit(1);
         }
     }
-    
+
     public static void main(String[] arguments) {
         new App().run();
     }
